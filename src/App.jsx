289b9a4de6f66a24141014a0fcc4611557dd54c6
Nandhakumar.V <nandhakumar.17cs@kct.ac.in>
2023-12-31 21:15:31 +0530

    UI Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import "./App.css";
 import About from "./components/About.jsx";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Product from "./components/Product.jsx";
-import ProductDetails from "./components/ProductDetails.jsx";
+import Product from "./components/Product/Product.jsx";
+import ProductDetails from "./components/Product/ProductDetails.jsx";
+import Layout from "./components/Layout.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/product">Products</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/product">
+              <Route index element={<Product />} />
+              <Route path=":id" element={<ProductDetails />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,25 +6,35 @@ const Product = () => {
   const [data, setData] = useState([]);
   const fetchproductData = async () => {
     const productData = await axios.get("https://fakestoreapi.com/products");
-    console.log(productData.data);
+    localStorage.setItem(
+      "local_product_data",
+      JSON.stringify(productData.data)
+    );
     setData(productData.data);
   };
 
   useEffect(() => {
-    fetchproductData();
+    const local_data = JSON.parse(localStorage.getItem("local_product_data"));
+    if (local_data != null) {
+      console.log("Data from Local");
+      setData(local_data);
+    } else {
+      console.log("Data from API");
+      fetchproductData();
+    }
   }, []);
 
   return (
-    <div className="container">
-      <div className="row">
-        {data.length > 0 ? (
-          data.map((product) => (
-            <div className="card" style={{ width: "18rem" }} key={product.id}>
+    <div className="row">
+      {data.length > 0 ? (
+        data.map((product) => (
+          <div className="col-3" key={product.id}>
+            <div className="card" style={{ width: "18rem" }}>
               <img
                 src={product.image}
                 className="card-img-top rounded"
                 style={{ width: "10rem" }}
-                alt="..."
+                alt="Image"
               />
               <div className="card-body">
                 <h5 className="card-title">{product.title}</h5>
@@ -34,11 +44,11 @@ const Product = () => {
                 </Link>
               </div>
             </div>
-          ))
-        ) : (
-          <div>Loading....</div>
-        )}
-      </div>
+          </div>
+        ))
+      ) : (
+        <div>Loading....</div>
+      )}
     </div>
   );
 };
